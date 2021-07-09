d9fac9fd468e20a7630d8d0e42146a6aecabe4ed
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-07-09 08:21:15 -0400

    get app display working

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
 mongo = PyMongo(app)
 
 @app.route("/")
 def index():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template('index.html', mars=mars)
 
 @app.route("/scrape")
 def scrape():
@@ -21,5 +21,5 @@ def scrape():
    mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
